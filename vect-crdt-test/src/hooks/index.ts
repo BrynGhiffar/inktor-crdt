55f94f57ba1e789c2f37275178b992f5d509fc4a
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2024-02-26 17:01:14 +0700

    Changes: * change crdt demo to include js version of library

--- vect-crdt-test/src/hooks/index.ts
+++ vect-crdt-test/src/hooks/index.ts
@@ -1,10 +1,33 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { flattenSVGDocTree, getSVGData, saveSVGData } from "../utility/methods";
-import { SVGDoc } from "@brynghiffar/vect-crdt-rs";
+// import { SVGDoc } from "@brynghiffar/vect-crdt-rs";
+import { SVGDoc as SVGDocRs } from "@brynghiffar/vect-crdt-rs";
+import { SVGDoc as SVGDocJs } from "@brynghiffar/vect-crdt-js";
 import { DroppableSVG } from "../types";
 
+
+const createSVGDoc = (docId: string, useJs: boolean = false) => {
+  if (!useJs) {
+    return SVGDocRs.new(docId)
+  } else {
+    return  SVGDocJs.new(docId);
+  }
+};
+
+const docs = new Map<string, SVGDocRs>();
+
+const getSVGDoc = (docId: string, useJs: boolean = false) => {
+  const doc = docs.get(docId);
+  if (!doc) {
+    const doc = createSVGDoc(docId, useJs);
+    docs.set(docId, doc);
+    return doc
+  }
+  return doc
+}
+
 export const useSVGDoc = (docId: string) => {
-  const SVGDocRef = useRef(SVGDoc.new(docId));
+  const SVGDocRef = useRef(getSVGDoc(docId));
   const [droppableSVG, setDroppableSVG] = useState<DroppableSVG[]>([]);
   const fetchSVGDoc = useCallback((initial?: boolean) => {
     const doc = SVGDocRef.current;
