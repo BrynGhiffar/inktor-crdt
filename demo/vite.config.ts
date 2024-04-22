62c81e861e39d7c747f32c7a08c144c8f7eefb7c
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2024-04-22 10:07:16 +0700

    add manual chunking

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react';
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    wasm(),
-    topLevelAwait(),
-  ],
-})
+  plugins: [react(), wasm(), topLevelAwait()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.match("dnd-kit")) {
+            return "dnd-kit";
+          }
+          if (id.match("zod")) {
+            return "zod";
+          }
+          if (id.match("react-colorful")) {
+            return "react-colorful";
+          }
+        },
+      },
+    },
+  },
+});
